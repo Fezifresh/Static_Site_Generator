f07ad51c70b5f66cbf8397abfac20dbe8dc2ad96
Fezifresh <felix.fassig@googlemail.com>
2024-11-11 22:47:16 +0100

    F: text_node_to_thml_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import LeafNode, HTMLNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -33,6 +33,19 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("I am a test file", TextType.CODE, "None")
         node2 = TextNode("I am a test file", TextType.CODE,)
         self.assertNotEqual(node, node2)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(text_node_to_html_node(node).to_html(), "<b>This is a text node</b>")
+
+    def test_eq_2(self):
+        node = TextNode("I am a test file", TextType.LINK, "www.google.com")
+        self.assertEqual(text_node_to_html_node(node).to_html(), "<a href=\"www.google.com\">I am a test file</a>")
+
+    def test_eq_3(self):
+        node = TextNode("I am a test image", TextType.IMAGE, "www.my.image.com")
+        self.assertEqual(text_node_to_html_node(node).to_html(), "<img src=\"www.my.image.com\" alt=\"I am a test image\"></img>")
 
 if __name__ == "__main__":
     unittest.main()
